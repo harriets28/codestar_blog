7a7f72d155a6dbd603b54665213c4cd17ffef369
harriets28 <harrietjsanderson@gmail.com>
2026-02-26 13:32:09 +0000

    deployment settings

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_Wzxi84PpcuEm@ep-bitter-base-alqkox2f.c-3.eu-central-1.aws.neon.tech/deed_clamp_opal_433031")
+
